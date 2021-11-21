691b333045ace9b28918cdfd768c9bf646729db3
Timur <mr14mr@yandex.ru>
2021-11-21 23:01:55 +0300

    working map snippet

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,33 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/weather',
-    name: 'Weather',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/WeatherPage.vue')
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/weather',
+        name: 'Weather',
+        component: () => import(/* webpackChunkName: "WeatherPage" */ '../views/WeatherPage.vue')
+    },
+    {
+        path: '/problem',
+        name: 'ReportProblemPage',
+        component: () => import(/* webpackChunkName: "ReportProblemPage" */ '../views/ReportProblemPage.vue')
+    },
+
+    {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
