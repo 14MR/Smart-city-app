152fa73870304320a397a332842817a274de054d
Timur <mr14mr@yandex.ru>
2021-11-21 23:35:51 +0300

    problem list page

---
+++ src/components/ReportProblem/ProblemList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="root">
+    <h3>Problems reported in this area:</h3>
+    <v-card
+        class="mx-auto problem-card"
+        max-width="344"
+        outlined
+        v-for="problem in problems"
+        v-bind:key="problem.name"
+    >
+      <v-list-item three-line>
+        <v-list-item-content>
+          <div class="text-overline mb-4">
+            Issue
+          </div>
+          <v-list-item-title class="text-h5 mb-1">
+            {{ problem.name }}
+          </v-list-item-title>
+          <v-list-item-subtitle>{{ problem.description }}</v-list-item-subtitle>
+        </v-list-item-content>
+
+        <v-list-item-content
+            tile
+
+        ><img style="max-width: 100%; width: 40px" :src="problem.picture_url"></v-list-item-content>
+      </v-list-item>
+
+      <v-card-actions>
+        <v-btn
+            outlined
+            rounded
+            text
+        >
+          Details
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ProblemList",
+  props: {
+    problems: Array
+  }
+}
+</script>
+
+<style scoped>
+#root {
+  padding: 20px 10px;
+}
+.problem-card{
+  margin: 5px;
+}
+</style>
--- src/views/ReportProblemPage.vue
+++ src/views/ReportProblemPage.vue
@@ -1,13 +1,19 @@
 <template>
-  <l-map style="height: 300px;" :zoom="zoom" :center="center">
-    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
-    <l-marker :lat-lng="markerLatLng"></l-marker>
-  </l-map>
+  <div>
+    <l-map style="height: 350px" :zoom="zoom" :center="center">
+      <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+      <l-marker :lat-lng="problem.markerLatLng" v-for="problem in problems" v-bind:key="problem.name">
+        <l-popup>{{ problem.name }}</l-popup>
+      </l-marker>
+    </l-map>
+
+    <ProblemList :problems="problems"/>
+  </div>
+
 </template>
 
 <script>
-import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
-import {Icon} from 'leaflet';
+import {Icon} from "leaflet";
 
 delete Icon.Default.prototype._getIconUrl;
 Icon.Default.mergeOptions({
@@ -15,12 +21,17 @@ Icon.Default.mergeOptions({
   iconUrl: require('leaflet/dist/images/marker-icon.png'),
   shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
 });
+import {LMap, LTileLayer, LMarker, LPopup} from 'vue2-leaflet';
+import ProblemList from "@/components/ReportProblem/ProblemList";
+
 export default {
   name: 'ReportProblemPage',
   components: {
+    ProblemList,
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    LPopup
   },
   data() {
     return {
@@ -29,10 +40,27 @@ export default {
           '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 20,
       center: [47.69548, 8.63317],
-      markerLatLng: [47.69548, 8.63317]
+      markerLatLng: [47.69548, 8.63317],
+      problems: [
+        {
+          name: 'Trashcan',
+          markerLatLng: [47.69548, 8.63317],
+          description: "There is an overflowed trashcan",
+          picture_url: 'https://libreshot.com/wp-content/uploads/2014/06/trash-can.jpg'
+        },
+        {
+          name: 'Problem 2',
+          markerLatLng: [47.695, 8.63317],
+          description: "There is a pitfall",
+          picture_url: 'https://www.fivestarautosalvage.co.uk/wp-content/uploads/2018/09/pothole.jpg'
+        }, {
+          name: 'Problem 3',
+          markerLatLng: [47.69548, 8.633],
+          description: "There is not clean street",
+          picture_url: 'https://www.thecourier.com.au/images/transform/v1/resize/frm/storypad-qtXAEZC67LbMeemRuC2bYX/a1f06b16-a939-4567-b556-c85aaf00f085.jpg/w1200_h678_fmax.jpg'
+        }
+      ]
     };
   }
 }
 </script>
-
-
