bd7cef621d83188098ab632559decf971f9c8718
Timur <mr14mr@yandex.ru>
2021-11-21 21:55:50 +0300

    init screen

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <v-app>
     <v-main>
-      <Card/>
+<!--      <template>-->
+<!--        <v-container class="grey lighten-5">-->
+<!--          <v-row>-->
+<!--            <v-col cols="12" sm="4">-->
+<!--              <Weather/>-->
+<!--            </v-col>-->
+<!--          </v-row>-->
+<!--          <v-row>-->
+<!--            <v-col-->
+<!--                v-for="n in 3"-->
+<!--                :key="n"-->
+<!--                cols="12"-->
+<!--                sm="4"-->
+<!--            >-->
+<!--              <Card/>-->
+<!--            </v-col>-->
+<!--          </v-row>-->
+<!--        </v-container>-->
+<!--      </template>-->
+    <Dashboard/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import Card from "@/components/Card";
+// import Card from "@/components/Card";
+// import Weather from "@/components/Weather";
+import Dashboard from "@/components/Dashboard";
 
 export default {
   name: 'App',
 
   components: {
-    Card
+    Dashboard,
+    // Weather,
+    // Card
   },
 
   data: () => ({
